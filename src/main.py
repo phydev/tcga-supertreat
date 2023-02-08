86e166e7ca5cdc5656e2246352f3998d3374baaf
Maurício Moreira-Soares <phydev@users.noreply.github.com>
2023-02-08 11:24:25 +0100

    add unittest and refactored main code

--- src/main.py
+++ src/main.py
@@ -6,108 +6,124 @@
 # then we need to download the files keeping the original file names
 
 import pandas as pd
+import warnings
 import requests
 import json
 import io
 
-manifest_file = '../data/manifest.json'
-
-# open manifest file with file names and UUIDs
-with open(manifest_file) as file:
-
-    # load json file as a dictionary
-    manifest = json.load(file)
-
-# create a dictionary with file UUIDs as keys and file names as values
-file_dict = {}
-
-for file in manifest:
-    file_dict[file["cases"][0]["case_id"]] = file['file_name']
-
-# load TGCA harmonized clinical data
-clin = pd.read_csv("../data/TCGA_harmonized.txt", sep=";")
-
-supertreat = pd.DataFrame(data={"case_uuid": clin["Patient_ID"]})
-
-
-
-# filter file_dict by supertreat cases
-file_dict = {k: v for k, v in file_dict.items() if k in supertreat["case_uuid"].values}
-
-# invert the dictionary to mapp file names to file UUIDs later
-inv_file_dict = {v: k for k, v in file_dict.items()}
-
-# GDC API endpoint for files
-files_endpt = "https://api.gdc.cancer.gov/files"
-
-# filter by file_name using all file names from the manifest file
-filters = {
-        "op": "=",
-        "content": {
-            "field": "file_name",
-            "value": list(file_dict.values())
-        }
-}
-
-params = {
-   "filters": json.dumps(filters),
-   "size": "1000"
-    }
-
-
-# download all files metadata from GDC to find the file UUIDs 
-response = requests.post(files_endpt, headers = {"Content-Type": "application/json"}, json = params)
-
-files_metadata = response.json()
-len(files_metadata["data"]["hits"])
-
-# download files
-
-# gene expression data format
-# each column will be identified with gene_id 
-# we will save unstranded, tpm_unstranded, fpkm_unstranded, fpkm_uq_unstranded in separate files
-# we will keep the mapping between gene_id and gene_name in a separate file, together with gene_type
-
-type_of_counts = "unstranded"
-
-gene_mapping = pd.DataFrame(columns = ["gene_id", "gene_name", "gene_type"])
-
-gene_expression = pd.DataFrame()
-
-number_of_files = len(files_metadata["data"]["hits"])
-
-for n, case in enumerate(files_metadata["data"]["hits"]):
-    print("Processing: {} of {}".format(n + 1, number_of_files))
-
-    file_uuid = case["file_id"]
+def get_data(file_uuid):
+    """
+    Download data from GDC API using the file UUID
+    :param file_uuid: file UUID
+    :return: dataframe with the data
+    """
     data_endpt = "https://api.gdc.cancer.gov/data/{}".format(file_uuid)
     response = requests.get(data_endpt,  headers = {"Content-Type": "application/json"})
+    df = pd.read_csv(io.StringIO(response.text), sep="\t", comment="#")
 
-    # select only the columns we need
-    df = pd.read_csv(io.StringIO(response.text), sep="\t", comment="#") 
+    return df
 
-    if not set(df["gene_id"].values).issubset(gene_expression.columns) and not gene_expression.empty:
-        print("Warning: New gene id found, NaN values will be added to the dataframe for previous cases.")
+def get_file_metadata(file_dict):
+    """
+    Download metadata from GDC API for the files in the file dictionary
+    :param file_dict: dictionary with file UUIDs as keys and file names as values
+    :return: dictionary with the file metadata
+    """
+    # GDC API endpoint for files
+    files_endpt = "https://api.gdc.cancer.gov/files"
 
-    data = dict(zip(df["gene_id"].values, df[type_of_counts].values))
-    gene_expression_temp = pd.DataFrame(data = data, index = [inv_file_dict[case["file_name"]]] )
-    gene_expression = pd.concat([gene_expression, gene_expression_temp])
+    # filter by file_name using all file names from the manifest file
+    filters = {
+                "op": "=",
+                "content": {
+                    "field": "file_name",
+                    "value": list(file_dict.values())
+                }
+            }
 
-    gene_mapping_temp = pd.DataFrame(
-                        data = {"gene_id": df["gene_id"].values, 
-                                "gene_name": df["gene_name"].values, 
-                                "gene_type": df["gene_type"].values}
-                                )
+    params = {
+            "filters": json.dumps(filters),
+            "size": "1000"
+            }
 
-    gene_mapping = pd.concat([gene_mapping, gene_mapping_temp])
-    gene_mapping.drop_duplicates(inplace=True)
-    # test if the values in data match with the ones in df["unstranded"]
+    # download all files metadata from GDC to find the file UUIDs 
+    response = requests.post(files_endpt, headers = {"Content-Type": "application/json"}, json = params)
+    files_metadata = response.json()
 
-    
-
-gene_expression.index.name = "Patient_ID"
-
-gene_expression.to_csv("../data/{}.csv".format(type_of_counts), sep=";")
+    return files_metadata
 
 
-gene_mapping.to_csv("../data/gene_mapping.csv", sep=";")
+if __name__ == "__main__":
+    # manifest file with file names and UUIDs
+    manifest_file = '../data/manifest.json'
+
+    # open manifest file with file names and UUIDs
+    with open(manifest_file) as file:
+
+        # load json file as a dictionary
+        manifest = json.load(file)
+
+    # create a dictionary with file UUIDs as keys and file names as values
+    file_dict = {}
+
+    for file in manifest:
+        file_dict[file["cases"][0]["case_id"]] = file['file_name']
+
+    # load TGCA harmonized clinical data
+    clin = pd.read_csv("../data/TCGA_harmonized.txt", sep=";")
+
+    supertreat = pd.DataFrame(data={"case_uuid": clin["Patient_ID"]})
+
+    # filter file_dict by supertreat cases
+    file_dict = {k: v for k, v in file_dict.items() if k in supertreat["case_uuid"].values}
+
+    # invert the dictionary to mapp file names to file UUIDs later
+    inv_file_dict = {v: k for k, v in file_dict.items()}
+
+    files_metadata = get_file_metadata(file_dict = file_dict)
+
+
+    # download files
+
+    # gene expression data format
+    # each column will be identified with gene_id 
+    # we will save unstranded, tpm_unstranded, fpkm_unstranded, fpkm_uq_unstranded in separate files
+    # we will keep the mapping between gene_id and gene_name in a separate file, together with gene_type
+
+    type_of_counts = "unstranded"
+
+    gene_mapping = pd.DataFrame(columns = ["gene_id", "gene_name", "gene_type"])
+
+    gene_expression = pd.DataFrame()
+
+    number_of_files = len(files_metadata["data"]["hits"])
+
+    for n, case in enumerate(files_metadata["data"]["hits"]):
+        print("Processing: {} of {}".format(n + 1, number_of_files))
+
+        # download data to a dataframe
+        df = get_data(file_uuid = case["file_id"])
+
+        if not set(df["gene_id"].values).issubset(gene_expression.columns) and not gene_expression.empty:
+            warnings.warn("New gene id found, NaN values will be added to the new gene column for previous cases.")
+
+        data = dict(zip(df["gene_id"].values, df[type_of_counts].values))
+        gene_expression_temp = pd.DataFrame(data = data, index = [inv_file_dict[case["file_name"]]] )
+        gene_expression = pd.concat([gene_expression, gene_expression_temp])
+
+        gene_mapping_temp = pd.DataFrame(
+                            data = {"gene_id": df["gene_id"].values, 
+                                    "gene_name": df["gene_name"].values, 
+                                    "gene_type": df["gene_type"].values}
+                                    )
+
+        gene_mapping = pd.concat([gene_mapping, gene_mapping_temp])
+        gene_mapping.drop_duplicates(inplace=True)
+
+
+    gene_expression.index.name = "Patient_ID"
+
+    gene_expression.to_csv("../data/{}.csv".format(type_of_counts), sep=";")
+
+
+    gene_mapping.to_csv("../data/gene_mapping.csv", sep=";")
---
+++ tests/test_data_integrity.py
@@ -0,0 +1,15 @@
+import unittest
+import pandas as pd
+import numpy as np
+from src.main import *
+
+class test_tcga_scraping(unittest.case):
+
+    def test_data_integrity(self):
+
+        df = get_data(file_uuid = "9eda745e-b182-4611-a4a6-242db8a07337")
+        data = dict(zip(df["gene_id"].values, df[type_of_counts].values))
+        gene_expression_temp = pd.DataFrame(data = data, index = [inv_file_dict[case["file_name"]]] )
+        gene_expression = pd.concat([gene_expression, gene_expression_temp])
+
+        self.assertIsNone(np.testing.assert_array_equal(gene_expression.values[0], df["unstranded"].values))
